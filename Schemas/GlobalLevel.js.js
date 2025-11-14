264b1a6109ca7507a192f0f42af37601fa061182
lode787 <van.santvliet.louis@gmail.com>
2025-11-14 05:23:58 +0100

    Update GlobalLevel.js.js

--- Schemas/GlobalLevel.js.js
+++ Schemas/GlobalLevel.js.js
@@ -1,1 +1,9 @@
-module.exports = require("./GlobalLevel.js");
+// schemas/GlobalLevel.js
+const { Schema, model } = require("mongoose");
+
+const globalLevelSchema = new Schema({
+  userId: { type: String, required: true },
+  balance: { type: Number, default: 0 },
+}, { timestamps: true });
+
+module.exports = model("GlobalLevel", globalLevelSchema);
